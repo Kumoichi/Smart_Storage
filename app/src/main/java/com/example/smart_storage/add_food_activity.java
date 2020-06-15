db3402576dc4b313e98a22cd274ef0d325444833
Julian Friesen <jubanator3000@gmail.com>
2020-06-15 16:20:27 -0700

    cleared all test buttons LOL added second activity to add food

---
+++ app/src/main/java/com/example/smart_storage/add_food_activity.java
@@ -0,0 +1,14 @@
+package com.example.smart_storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_food_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_activity);
+    }
+}
