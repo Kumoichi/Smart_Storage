74f4dc5e94994df4a14c8d9b4953c30480a13477
Victor <vjrtom@gmail.com>
2020-06-15 17:29:33 -0700

    made my_row layout

--- app/src/main/java/MyAdapter.java
+++ app/src/main/java/MyAdapter.java
@@ -1,9 +1,36 @@
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String title[], expiration[];
+    Context context;
+
+    public MyAdapter(Context c, String s1[], String s2[]) {
+        context = c;
+        title = s1;
+        expiration = s2;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
