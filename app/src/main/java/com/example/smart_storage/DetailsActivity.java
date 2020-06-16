1298f1fd1eda8b88efd14cb7af7e7694b391ece2
Victor <vjrtom@gmail.com>
2020-06-15 20:54:09 -0700

    added DetailsActivity

---
+++ app/src/main/java/com/example/smart_storage/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
