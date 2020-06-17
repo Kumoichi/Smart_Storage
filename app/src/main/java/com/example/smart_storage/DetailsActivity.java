75e6c4b2df249c3da76db231724a17bdd6d5b37d
Julian Friesen <jubanator3000@gmail.com>
2020-06-16 17:31:55 -0700

    details activity return button functional

--- app/src/main/java/com/example/smart_storage/DetailsActivity.java
+++ app/src/main/java/com/example/smart_storage/DetailsActivity.java
@@ -1,6 +1,9 @@
 package com.example.smart_storage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -8,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    Button returnButton, deleteButton;
     TextView detailTitle, detailExpiry, detailStorage;
     String str1, str2, str3; //temp strings to transfer to textview
 
@@ -19,9 +23,23 @@ public class DetailsActivity extends AppCompatActivity {
         detailTitle = findViewById(R.id.detailTitle);
         detailExpiry = findViewById(R.id.detailExpiry);
         detailStorage = findViewById(R.id.detailStorage);
+        returnButton = findViewById(R.id.detailReturn);
+        deleteButton = findViewById(R.id.detailDelete);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMain();
+            }
+        });
 
         getData();
         setData();
+    }
+
+    private void returnToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void getData() {
